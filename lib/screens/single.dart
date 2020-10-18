25e6e3471c196bfb7b080442d8d13cae325e5121
Navid Shad <navidshad72@gmail.com>
2020-10-18 22:09:03 +0330

    added movment effect for car prevew in single page

--- lib/screens/single.dart
+++ lib/screens/single.dart
@@ -8,10 +8,60 @@ class SinglePage extends StatefulWidget {
 }
 
 class _SinglePageState extends State<SinglePage> {
+  ScrollController cardDetailScrollController;
+  bool firstFrameRendered = false;
+  double carXBasePosition = 0;
+  double carXAditionalPositionForAnimation = 0;
+  double carImageWidth = 100;
+
+  initState() {
+    super.initState();
+    cardDetailScrollController = ScrollController();
+    cardDetailScrollController.addListener(onCardDetailScrollChanged);
+  }
+
+  onCardDetailScrollChanged() {
+    // Calculate cardDetail scroll percentage
+    double cardDetailScrollPosition = cardDetailScrollController.offset;
+    double cardDetailWidth =
+        cardDetailScrollController.position.maxScrollExtent;
+    double cardDetailScrollPercent =
+        cardDetailScrollPosition / (cardDetailWidth / 100);
+
+    // Calculate new value for car image x position
+    carXAditionalPositionForAnimation = cardDetailScrollPercent * (carImageWidth/150);
+    
+    // Build new Frame
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: LayoutBuilder(builder: (context, constraints) {
+        // Create ScrolView for CardDetails
+        double cardDetailBoxSize = constraints.maxWidth / 4;
+        carImageWidth = constraints.maxWidth * 1.4;
+
+        ListView cardDetails = ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          controller: cardDetailScrollController,
+          itemBuilder: (context, index) {
+            return Container(
+              width: cardDetailBoxSize,
+              height: cardDetailBoxSize,
+              margin: EdgeInsets.all(10),
+              color: Colors.yellow,
+            );
+          },
+        );
+
+        if (!firstFrameRendered) {
+          firstFrameRendered = true;
+          carXBasePosition = -carImageWidth / 2;
+        }
+
         return SizedBox(
             width: constraints.maxWidth,
             height: constraints.maxHeight,
@@ -53,12 +103,22 @@ class _SinglePageState extends State<SinglePage> {
 
                 // CAR IMAGE
                 Positioned(
-		left: 100,
+                  right: carXBasePosition + carXAditionalPositionForAnimation,
                   child: Container(
                       child: Image.asset(
                     'assets/images/car-01.png',
-                    width: constraints.maxWidth * 1.4,
+                    width: carImageWidth,
                   )),
+                ),
+
+                // CARD SCROLLER
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    width: constraints.maxWidth,
+                    height: cardDetailBoxSize,
+                    child: cardDetails,
+                  ),
                 )
               ],
             ));
